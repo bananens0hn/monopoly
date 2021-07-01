8cce016b16cb1b06eefc2d00c1cc9a69218f5086
yannick.specht <yannick.specht@cr1-pc11.ad.egf.logodidact.net>
2021-07-01 09:03:20 +0200

    player and dice

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,17 @@
+import java.util.Random;
 
 public class Dice {
 
+	public Dice() {
+		
+	}
+	
+	
+	Random numberGenerator = new Random(); 
+	
+	public int roll() {
+		return numberGenerator.nextInt(6) + 1 + numberGenerator.nextInt(6) + 1;
+	}
+	
+	
 }
--- src/Player.java
+++ src/Player.java
@@ -4,10 +4,47 @@ public class Player {
 	public String figure;
 	public int playerNum;
 	public String playerName;
+	public int playerBalance;
 	
-	public Player(int num, String name, String fig) {
+	public Player(int num, String name, String fig, int balance) {
 		figure = fig;
 		playerNum = num;
 		playerName = name;
+		playerBalance = balance;
+	}
+	
+	//Money
+	public int getPlayerBalance(){
+		return playerBalance;
+	}
+	
+	public void setPlayerBalance(int balance) {
+		playerBalance = balance;
+	}
+	
+	public void increasePlayerBalance(int add) {
+		setPlayerBalance(getPlayerBalance() + add);
+	}
+	
+	public void decreasePlayerBalance(int dec) {
+		setPlayerBalance(getPlayerBalance() - dec);
+	}
+	
+	
+	//Position
+	public void setPosition(int num) {
+		position = num;
+	}
+	
+	public int getPosition() {
+		return position;
+	}
+	
+	public void goForward(int num) {
+		setPosition(getPosition() + num);
+	}
+	
+	public void goBack(int num) {
+		setPosition(getPosition() - num);	
 	}
 }
