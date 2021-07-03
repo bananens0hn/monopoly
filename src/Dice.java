42907b52f7e66f4e17e3ae8136d16ffd571e199a
Michael <Michael@DESKTOP-BUDABTC.Speedport_W_724V_Typ_A_05011603_06_003>
2021-07-03 12:07:12 +0200

    Player and Dice in benni branch

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,14 @@
+import java.util.Random;
 
 public class Dice {
-
+	public Dice() {
+		
+	}
+	
+	
+	Random numberGenerator = new Random(); 
+	
+	public int roll() {
+		return numberGenerator.nextInt(6) + 1 + numberGenerator.nextInt(6) + 1;
+	}
 }
