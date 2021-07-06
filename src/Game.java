4cfa2e3612923f38b11f208940d6af5bb8d56850
Thomas.Michl <Thomas.Michl@cr1-pc07.ad.egf.logodidact.net>
2021-07-06 11:24:15 +0200

    benni hat kleine schwonz

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,7 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Game {
@@ -5,16 +9,32 @@ public class Game {
 	int windowWidth = 1080;
 	int windowHeight = 720;
 	
+	JButton btnStart;
+	
 	Player player1;
 	
 	public Game() {
 		player1 = new Player(0, "hans", "Kreis", 0);
+		
+		//set Button
+		btnStart = new JButton("Start");
+		
+		 btnStart.addActionListener(new ActionListener(){
+	    	  public void actionPerformed(ActionEvent e){
+	    		  btnStart.setVisible(false);
+	    		  // Vielleicht ein Käfer Fehler
+	    		  frame.add(new Board());
+	    	             
+	    	  }
+	      }
+	      );
+		
 	}
 	
 	public void setupGame() {
 		frame = new JFrame("fortnite");
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.add(new Board());
+		frame.add(btnStart);
 		frame.setUndecorated(true);
 		frame.setVisible(true);
 	}
